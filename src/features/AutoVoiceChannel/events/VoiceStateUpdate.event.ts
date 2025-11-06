b6378d4e40a00ea7967e97ef2b8b42a4c9782edd
ImJustNon <Im_Just_Non@hotmail.com>
2025-11-06 18:52:32 +0700

    Migrated: AutoVoiceChannel Core use mongo

--- src/features/AutoVoiceChannel/events/VoiceStateUpdate.event.ts
+++ src/features/AutoVoiceChannel/events/VoiceStateUpdate.event.ts
@@ -1,16 +1,22 @@
 import { ChannelType, PermissionsBitField, VoiceChannel, VoiceState } from "discord.js";
 import { BotClient } from "../../../classes/Client.class";
+import { GuildAutoVoiceChannel } from "../../../models/GuildAutoVoiceChannel.model";
+import { CacheAutoVoiceChannel } from "../../../models/CacheAutoVoiceChannel.model";
 
 export default class VoiceStateUpdate {
     constructor(client: BotClient){
         client.on("voiceStateUpdate", async(oldState: VoiceState, newState: VoiceState) =>{
             // Join Channel
             if(newState.channel && !oldState.channel){
-                const findAutoVCConfig = await client.prisma.guildAutoVoiceChannel.findUnique({
-                    where: {
-                        guild_id: newState.guild.id,
-                        channel_id: newState.channel.id
-                    }
+                // const findAutoVCConfig = await client.prisma.guildAutoVoiceChannel.findUnique({
+                //     where: {
+                //         guild_id: newState.guild.id,
+                //         channel_id: newState.channel.id
+                //     }
+                // });
+                const findAutoVCConfig = await GuildAutoVoiceChannel.findOne({
+                    guild_id: newState.guild.id,
+                    channel_id: newState.channel.id
                 });
                 if(!findAutoVCConfig) return;
                 await this.createVoiceChannel(client, newState);
@@ -18,21 +24,29 @@ export default class VoiceStateUpdate {
             }
             // Left Channel
             if(oldState.channel && !newState.channel){
-                const findAutoVCCache = await client.prisma.cacheAutoVoiceChannel.findUnique({
-                    where: {
-                        guild_id: oldState.guild.id,
-                        channel_id: oldState.channel.id
-                    }
+                // const findAutoVCCache = await client.prisma.cacheAutoVoiceChannel.findUnique({
+                //     where: {
+                //         guild_id: oldState.guild.id,
+                //         channel_id: oldState.channel.id
+                //     }
+                // });
+                const findAutoVCCache = await CacheAutoVoiceChannel.findOne({
+                    guild_id: oldState.guild.id,
+                    channel_id: oldState.channel.id
                 });
                 if(!findAutoVCCache) return;
                 const voiceChannel = oldState.guild.channels.cache.get(findAutoVCCache.channel_id);
                 if(!voiceChannel) return;
                 if((voiceChannel as VoiceChannel).members.size >= 1) return;
-                await client.prisma.cacheAutoVoiceChannel.delete({
-                    where: {
-                        guild_id: oldState.guild.id,
-                        channel_id: oldState.channel.id
-                    }
+                // await client.prisma.cacheAutoVoiceChannel.delete({
+                //     where: {
+                //         guild_id: oldState.guild.id,
+                //         channel_id: oldState.channel.id
+                //     }
+                // });
+                await CacheAutoVoiceChannel.deleteOne({
+                    guild_id: oldState.guild.id,
+                    channel_id: oldState.channel.id
                 });
                 await voiceChannel.delete().catch(e => {});
                 return;
@@ -40,31 +54,43 @@ export default class VoiceStateUpdate {
             // Switch Channel
             if(oldState.channel && newState.channel){
                 if(oldState.channel.id === newState.channel.id) return;
-                const findAutoVCConfig = await client.prisma.guildAutoVoiceChannel.findUnique({
-                    where: {
-                        guild_id: newState.guild.id,
-                        channel_id: newState.channel.id
-                    }
+                // const findAutoVCConfig = await client.prisma.guildAutoVoiceChannel.findUnique({
+                //     where: {
+                //         guild_id: newState.guild.id,
+                //         channel_id: newState.channel.id
+                //     }
+                // });
+                const findAutoVCConfig = await GuildAutoVoiceChannel.findOne({
+                    guild_id: newState.guild.id,
+                    channel_id: newState.channel.id
                 });
                 if(findAutoVCConfig){
                     await this.createVoiceChannel(client, newState);
                 }
-                const findAutoVCCache = await client.prisma.cacheAutoVoiceChannel.findUnique({
-                    where: {
-                        guild_id: oldState.guild.id,
-                        channel_id: oldState.channel.id
-                    }
+                // const findAutoVCCache = await client.prisma.cacheAutoVoiceChannel.findUnique({
+                //     where: {
+                //         guild_id: oldState.guild.id,
+                //         channel_id: oldState.channel.id
+                //     }
+                // });
+                const findAutoVCCache = await CacheAutoVoiceChannel.findOne({
+                    guild_id: oldState.guild.id,
+                    channel_id: oldState.channel.id
                 });
                 if(!findAutoVCCache) return;
                 const voiceChannel = oldState.guild.channels.cache.get(findAutoVCCache.channel_id);
                 if(!voiceChannel) return;
                 if((voiceChannel as VoiceChannel).members.size >= 1) return;
-                await client.prisma.cacheAutoVoiceChannel.delete({
-                    where: {
-                        guild_id: oldState.guild.id,
-                        channel_id: oldState.channel.id
-                    }
-                });
+                // await client.prisma.cacheAutoVoiceChannel.delete({
+                //     where: {
+                //         guild_id: oldState.guild.id,
+                //         channel_id: oldState.channel.id
+                //     }
+                // });
+                await CacheAutoVoiceChannel.deleteOne({
+                    guild_id: oldState.guild.id,
+                    channel_id: oldState.channel.id
+                })
                 await voiceChannel.delete().catch(e => {});
                 return;
             }
@@ -80,13 +106,19 @@ export default class VoiceStateUpdate {
             parent: channelParent,
         }).then(async newVoiceChannel =>{
             await state.member?.voice.setChannel(newVoiceChannel);
-            await client.prisma.cacheAutoVoiceChannel.create({
-                data: {
-                    guild_id: state.guild.id,
-                    channel_id: state.channel!.id,
-                    parent_id: channelParent !== null ? channelParent : "0",
-                    creator_user_id: state.member!.id,
-                }
+            // await client.prisma.cacheAutoVoiceChannel.create({
+            //     data: {
+            //         guild_id: state.guild.id,
+            //         channel_id: state.channel!.id,
+            //         parent_id: channelParent !== null ? channelParent : "0",
+            //         creator_user_id: state.member!.id,
+            //     }
+            // });
+            await CacheAutoVoiceChannel.create({
+                guild_id: state.guild.id,
+                channel_id: state.channel!.id,
+                parent_id: channelParent !== null ? channelParent : "0",
+                author_id: state.member!.id,
             });
             await newVoiceChannel.permissionOverwrites.set([
                 {
