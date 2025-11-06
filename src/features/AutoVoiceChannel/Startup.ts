2946eb5bcc71e782f3079b20640d100a76b649b8
ImJustNon <Im_Just_Non@hotmail.com>
2025-11-06 18:55:15 +0700

    Migrated: AutoVoiceChannel Core use mongo

--- src/features/AutoVoiceChannel/Startup.ts
+++ src/features/AutoVoiceChannel/Startup.ts
@@ -1,43 +1,57 @@
 import { ChannelType, Guild, GuildBasedChannel, VoiceBasedChannel } from "discord.js";
 import { BotClient } from "../../classes/Client.class";
+import { CacheAutoVoiceChannel } from "../../models/CacheAutoVoiceChannel.model";
 
 export default class AutoVoiceChannelStartup {
     constructor(client: BotClient){
         new Promise(async resolve =>{
             setTimeout(async() => {
                 try {
-                    const findAllAutoVCCache = await client.prisma.cacheAutoVoiceChannel.findMany();
+                    // const findAllAutoVCCache = await client.prisma.cacheAutoVoiceChannel.findMany();
+                    const findAllAutoVCCache = await CacheAutoVoiceChannel.find();
                     
                     for(const vc of findAllAutoVCCache){
                         const guild: Guild | undefined = client.guilds.cache.get(vc.guild_id);
                         if(!guild) { 
                             // if guild not found then delete all guild cache data and skip
-                            await client.prisma.cacheAutoVoiceChannel.deleteMany({
-                                where: {
-                                    guild_id: vc.guild_id
-                                }
+
+                            // await client.prisma.cacheAutoVoiceChannel.deleteMany({
+                            //     where: {
+                            //         guild_id: vc.guild_id
+                            //     }
+                            // });
+                            await CacheAutoVoiceChannel.deleteMany({
+                                guild_id: vc.guild_id
                             });
                             continue;
                         }; 
 
                         const voiceChannel: GuildBasedChannel | undefined = guild.channels.cache.get(vc.channel_id);
                         if(!voiceChannel){
-                            await client.prisma.cacheAutoVoiceChannel.delete({
-                                where: {
-                                    guild_id: vc.guild_id,
-                                    channel_id: vc.channel_id
-                                }
+                            // await client.prisma.cacheAutoVoiceChannel.delete({
+                            //     where: {
+                            //         guild_id: vc.guild_id,
+                            //         channel_id: vc.channel_id
+                            //     }
+                            // });
+                            await CacheAutoVoiceChannel.deleteOne({
+                                guild_id: vc.guild_id,
+                                channel_id: vc.channel_id
                             });
                             continue;
                         }
                         
                         if(voiceChannel.type === ChannelType.GuildVoice){
                             if((voiceChannel as VoiceBasedChannel).members.size < 1){
-                                await client.prisma.cacheAutoVoiceChannel.delete({
-                                    where: {
-                                        guild_id: vc.guild_id,
-                                        channel_id: vc.channel_id
-                                    }
+                                // await client.prisma.cacheAutoVoiceChannel.delete({
+                                //     where: {
+                                //         guild_id: vc.guild_id,
+                                //         channel_id: vc.channel_id
+                                //     }
+                                // });
+                                await CacheAutoVoiceChannel.deleteOne({
+                                    guild_id: vc.guild_id,
+                                    channel_id: vc.channel_id
                                 });
                                 await voiceChannel.delete().catch(() => {});
                             }
