d0afa24476c57012e6e387d316583cae1aa53ed9
ImJustNon <Im_Just_Non@hotmail.com>
2025-11-09 15:34:44 +0700

    fix: add protected client property to Event base class

--- src/features/ServerLogging/classes/Event.class.ts
+++ src/features/ServerLogging/classes/Event.class.ts
@@ -3,7 +3,11 @@ import { BotClient } from "../../../classes/Client.class";
 import { GuildLoggingChannel } from "../../../models/GuildLoggingChannel.model";
 
 export class Event {
+    protected event: keyof ClientEvents;
+    protected client: BotClient;
     constructor(client: BotClient, event: keyof ClientEvents) {
+        this.event = event;
+        this.client = client;
         client.on(event, (...args) => this.callback(...args));
     }
 
